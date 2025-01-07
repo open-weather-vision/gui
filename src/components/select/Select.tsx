79ac47321e8290717c4c9d781da6229254459d59
harrydehix <harry.holly02@gmail.com>
2025-01-07 18:38:29 +0100

    Small fixes

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import styles from "./Select.module.css";
 import { ReactComponent as DownArrow } from "../../img/icons/down-arrow.svg"
 
@@ -19,34 +19,45 @@ export function fromOptionsArray(optionsArray: {label: React.ReactNode | string,
 export default function Select(props: SelectProps) {
     const [selectedValue, setSelectedValue] = useState(props.value ?? Object.keys(props.options)[0]);
     const [showOptions, setShowOptions] = useState(false);
-
-
-
-    function toggleChoices(event: React.MouseEvent<HTMLButtonElement, MouseEvent>){
-        const div = event.target as HTMLDivElement
-        setShowOptions(!showOptions)
-        clearTimeout(focusLooseTimer);
-    }
+    const ref = useRef(null);
 
     function selectOption(option: string){
         setSelectedValue(option);
         if(props.onSelect) props.onSelect(option);
+        
+        blur();
+    }
+
+    function focus(){
+        if(!ref.current) return;
+        const div = ref.current as HTMLDivElement
+        div.focus()
+    }
+
+    function blur(){
+        if(!ref.current) return;
+        const div = ref.current as HTMLDivElement
+        div.blur()
+    }
+
+    function onBlur(event: any){
+        event.stopPropagation()
+        event.preventDefault()
         setShowOptions(false)
     }
 
-    const [focusLooseTimer, setFocusLooseTimer] = useState<any>();
-    function onFocusLoose(){
-        setFocusLooseTimer(setTimeout(() => {
-            setShowOptions(false);
-        }, 500));
+    function onFocus(event: any){
+        event.stopPropagation()
+        event.preventDefault()
+        setShowOptions(true);
     }
 
     return (
-        <div className={styles.select}>
-            <button className={styles.selection} onBlur={onFocusLoose} onClick={toggleChoices} tabIndex={1}>
+        <div className={styles.select} ref={ref} onFocus={onFocus} onBlur={onBlur} tabIndex={1}>
+            <div className={styles.selection}>
                 <div className={styles.label}>{props.options[selectedValue]}</div>
                 <DownArrow className={styles.arrow} />
-            </button>
+            </div>
             <div className={`${styles.options} ${showOptions && styles.visible}`}>
                 {Object.keys(props.options).map((optionValue, index) => <div key={index} className={`${styles.option} ${selectedValue === optionValue && styles.selected}`} onClick={() => selectOption(optionValue)}>
                     {props.options[optionValue]}
